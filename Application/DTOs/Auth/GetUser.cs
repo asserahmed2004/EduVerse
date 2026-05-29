47efbf462e34d62d8ea735af0872ba77c569d803
Asser Ahmed <asserahmed2004@gmail.com>
2026-05-29 04:34:12 +0300

    addin id to getuser

--- Application/DTOs/Auth/GetUser.cs
+++ Application/DTOs/Auth/GetUser.cs
@@ -8,6 +8,7 @@ namespace Application.DTOs.Auth
 {
     public class GetUser
     {
+        public string Id { get; set; }
         public string FullName { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
